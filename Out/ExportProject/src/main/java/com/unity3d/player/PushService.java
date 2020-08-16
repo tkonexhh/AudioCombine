73f506808c86dd2d07e1c55cd96e1ddc2fd66b24
neo.xu <neo.xu@modooplay.com>
2020-08-16 09:16:35 +0800

    Add android project

---
+++ Out/ExportProject/src/main/java/com/unity3d/player/PushService.java
@@ -0,0 +1,8 @@
+package com.unity3d.player;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
---
+++ Out/ExportProject/src/main/java/com/unity3d/player/UnityApplication.java
@@ -0,0 +1,45 @@
+package com.unity3d.player;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.multidex.MultiDex;
+import android.util.Log;
+
+import cn.jpush.android.api.JPushInterface;
+
+
+//import com.ugame.sdkbox.UGameMultiApp;
+
+/**
+ * Created by Administrator on 2019/5/5.
+ */
+
+public class UnityApplication extends Application {
+    public static Activity mUnityActivity;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+        Log.e("package",getPackageName());
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+}
